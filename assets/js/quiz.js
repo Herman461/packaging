9778e9ea28485688f1a86eb3c35ca897c3dd5a84
Herman Zhukovskiy <herman461@Hermans-MacBook-Air.local>
2026-04-29 19:14:17 +0300

    add quiz and course intro pages

---
+++ assets/js/quiz.js
@@ -0,0 +1,48 @@
+
+window.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.quiz__slider', {
+        allowTouchMove: false,
+        allowSlidePrev: false,
+        slidesPerView: 1,
+        effect: 'fade',
+        speed: 500,
+        autoHeight: true,
+        breakpoints: {
+            767.98: {
+                autoHeight: false
+            }
+        },
+        fadeEffect: {
+            crossFade: true,
+        },
+    });
+
+    const currentEl = document.querySelector('.quiz__current');
+    const totalEl = document.querySelector('.quiz__total');
+    const progressBar = document.querySelector('.quiz__progress span');
+
+    const totalSlides = swiper.slides.length;
+    totalEl.textContent = String(totalSlides).padStart(2, '0');
+
+    totalEl.textContent = String(swiper.slides.length).padStart(2, '0');
+
+    function updateQuizProgress() {
+        const currentSlide = swiper.realIndex + 1;
+
+        currentEl.textContent = String(currentSlide).padStart(2, '0');
+
+        const progressPercent = (currentSlide / totalSlides) * 100;
+        progressBar.style.width = `${progressPercent}%`;
+    }
+
+    updateQuizProgress();
+
+    swiper.on('slideChange', updateQuizProgress);
+
+    document.querySelectorAll('.slide-quiz__option input[type="radio"]').forEach((radio) => {
+        radio.addEventListener('change', () => {
+            swiper.slideNext();
+        });
+    });
+
+})
